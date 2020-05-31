8519da30cc35f682b99ad52e9a4123d19d8c20df
Anuraj Deep <deep.anuraj10@gmail.com>
2020-05-31 18:55:40 +0530

    github link completed

--- app/src/main/java/com/example/to_do_app/TaskActivity.java
+++ app/src/main/java/com/example/to_do_app/TaskActivity.java
@@ -3,14 +3,15 @@ package com.example.to_do_app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 public class TaskActivity extends AppCompatActivity {
 
-    Button add_task;
-    Button show_task;
+    Button add_task, show_task, github;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,5 +32,18 @@ public class TaskActivity extends AppCompatActivity {
                 startActivity(new Intent(TaskActivity.this, ShowActivity.class));
             }
         });
+
+        github = (Button) findViewById(R.id.github);
+        github.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openUrl("https://github.com/anuraj07");
+            }
+        });
+    }
+    public void openUrl(String url) {
+        Uri uri = Uri.parse(url);
+        Intent launchGit = new Intent(Intent.ACTION_VIEW, uri);
+        startActivity(launchGit);
     }
 }
